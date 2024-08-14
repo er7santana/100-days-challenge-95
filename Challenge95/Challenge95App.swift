cad079628b2229e4f12c9db1a9233656df2da8c1
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-14 19:54:30 -0300

    Initial Commit

---
+++ Challenge95/Challenge95App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge95App.swift
+//  Challenge95
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge95App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
