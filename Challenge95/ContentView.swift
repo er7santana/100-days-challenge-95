218bbd936be19356914e35624b8208c0b47f9813
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-14 21:03:52 -0300

    Initial Commit

--- Challenge95/ContentView.swift
+++ Challenge95/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var oldValues = [Int]()
+    @State private var currentValue = 0
+    @State private var isRollingDice = false
+    @State private var numberOfTries = 0
+    @State private var timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                
+                Image(systemName: currentValue > 0 ? "die.face.\(currentValue).fill" : "questionmark.diamond.fill")
+                    .renderingMode(.template)
+                    .font(.system(size: 140))
+                    .foregroundStyle(.orange)
+                    .frame(height: 220)
+                
+                Button(action: {
+                    startRollingDice()
+                }, label: {
+                    Text("Roll dice")
+                        .padding()
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .opacity(isRollingDice ? 0.3 : 1.0)
+                        .clipShape(.capsule)
+                })
+                .disabled(isRollingDice)
+                
+                if oldValues.isEmpty == false {
+                    HStack {
+                        Text("Old values")
+                            .font(.headline)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    List {
+                        ForEach(oldValues, id: \.self) { item in
+                            Text(String(item))
+                        }
+                    }
+                    .listStyle(.plain)
+                }
+                Spacer()
+            }
+            .navigationTitle("Dice App")
+            .onReceive(timer) { _ in
+                guard isRollingDice else { return }
+                
+                numberOfTries += 1
+                
+                rollDice()
+                if numberOfTries == 10 {
+                    isRollingDice = false
+                    
+                    oldValues.insert(currentValue, at: 0)
+                    timer.upstream.connect().cancel()
+                }
+            }
         }
-        .padding()
+    }
+    
+    func rollDice() {
+        withAnimation {
+            currentValue = Int.random(in: 1...6)
+        }
+    }
+    
+    func startRollingDice() {
+        numberOfTries = 0
+        isRollingDice = true
+        timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
     }
 }
 
